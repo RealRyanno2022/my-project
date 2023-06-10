30387313c16d7b200d10955611024eecdb555944
Daniel Ryan <ryannobusiness@gmail.com>
2023-06-10 14:39:24 +0100

    Fixing the podfile

--- react-native.config.js
+++ react-native.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-    dependency: {
-      platforms: {
-        android: null,
-        ios: null,
-      },
+  dependency: {
+    platforms: {
+      android: null,
+      ios: null,
+      ...(process.env.NO_FLIPPER === '1'
+        ? {}
+        : {'react-native-flipper': { platforms: ['android', 'ios'] }}),
     },
-    commands: [],
-  };
+  },
+  commands: [],
+};
